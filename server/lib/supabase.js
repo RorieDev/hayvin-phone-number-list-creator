1f7dc2d2b74f9afeb355f7c7517b2c2903980bd2
Rorie Devine <rorie.devine@gmail.com>
2026-02-07 13:00:35 +0000

    Allow server to load local environment overrides

--- server/lib/supabase.js
+++ server/lib/supabase.js
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+dotenv.config({ path: '.env.local', override: true });
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
